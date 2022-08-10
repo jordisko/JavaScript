2058ee00b9315137bba0e938e039a839440a17f5
jordi campos <jordicampos.97@gmail.com>
2022-08-10 12:30:44 +0200

    creacion de carpeta y finalización de Ejercicio3

---
+++ Ejercicio3/main.js
@@ -0,0 +1,24 @@
+let nombre = "Jordi";
+let numero = 21;
+let booleano = true;
+let nada = null;
+let undefinido =  undefined;
+let symbolo = Symbol("jordi");
+let objeto = { nombre: "jordi", edad: 24, dni: "35234324s"};
+let array = ["jordi", 24, "35234324s", false ];
+console.log(`${nombre}\n${numero}\n${booleano}\n${nada}\n${undefinido}\n${objeto}\n${array} `);
+console.log(symbolo);
+console.log(typeof nombre);
+console.log(typeof numero);
+console.log(typeof booleano);
+console.log(typeof nada);
+console.log(typeof undefinido);
+console.log(typeof symbolo);
+console.log(typeof objeto);
+console.log(typeof array);
+
+if (true) {
+    numero = 22;
+    console.log(numero)
+}
+
