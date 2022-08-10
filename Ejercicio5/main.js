c02dcf9d797a2af19a549cbc83c4e9ad201e21f4
jordi campos <jordicampos.97@gmail.com>
2022-08-10 13:55:16 +0200

    Ejercicio5 finalizado

---
+++ Ejercicio5/main.js
@@ -0,0 +1,14 @@
+function retorna0o1 (){
+    let i = Math.round(Math.random());
+    console.log(i);
+    
+    if (i == 0){
+        console.log("cara")
+        
+    }
+    else if (i==1){
+        console.log("cruz")
+    }
+    
+}
+retorna0o1()
