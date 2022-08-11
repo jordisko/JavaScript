2f12b143a7c91cf9c7374b38ba410a1c7bd9c8c1
Jordi Campos <jordicampos.97@gmail.com>
2022-08-11 20:38:51 +0200

    Ejercicio11 finalizado

---
+++ Ejercicio11/main.js
@@ -0,0 +1,11 @@
+1//Bucle con for:
+for (let i = 0; i <10; i++){
+    console.log("I love code");
+}
+2//Bucle con while:
+let i = 0;
+while (i<10) {
+    console.log("I love code");
+    i++
+    
+}
