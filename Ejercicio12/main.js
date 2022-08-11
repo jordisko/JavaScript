ecbd1a9b90049e6df0416de9bf00c4f36bf1c76e
Jordi Campos <jordicampos.97@gmail.com>
2022-08-11 20:53:46 +0200

    Ejercicio12 finalizado

---
+++ Ejercicio12/main.js
@@ -0,0 +1,10 @@
+1//
+array1= ["a","b","c","d","e"];
+array1.forEach(element => {
+    console.log(element);
+});
+2//
+for (let numero = 5; numero >= 0; numero--) {
+    console.log(numero);
+    
+}
